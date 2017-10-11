f447d8f043089272fea69273d6ff3ca56dc477e2
Neil Swainston <neil.swainston@manchester.ac.uk>
2017-10-11 14:54:45 +0100

    Added specification of mutant oligo pools.

--- geneorator/combinatorial.py
+++ geneorator/combinatorial.py
@@ -9,23 +9,24 @@ import itertools
 import sys
 
 
-def combine(oligos, n_mutated=0, n_blocks=1):
+def combine(oligos, n_mutated=0, mutant_oligos=None, n_blocks=1):
     '''Design combinatorial assembly.'''
 
     # Assertion sanity checks:
     assert(len(oligos) % 2 == 0)
     assert(len(oligos) / n_blocks >= 2)
+    assert(mutant_oligos if n_mutated > 0 else True)
 
     designs = []
 
     # Get combinations:
-    combis = itertools.combinations(oligos, n_mutated)
+    combis = itertools.combinations(mutant_oligos, n_mutated)
 
     for combi in combis:
         design = list(oligos)
 
         for var in combi:
-            design[design.index(var)] = var + 'm'
+            design[design.index(var[0])] = var[1]
 
         block_lengths = [0] * n_blocks
 
@@ -56,9 +57,10 @@ def _pairwise(iterable):
 
 def main(args):
     '''main method.'''
-    oligos = [str(val) for val in range(1, 7)]
+    oligos = [str(val) for val in range(1, 29)]
+    mutant_oligos = ((oligo, oligo + 'm') for oligo in oligos)
 
-    for design in combine(oligos, int(args[0]), int(args[1])):
+    for design in combine(oligos, int(args[0]), mutant_oligos, int(args[1])):
         print design
 
 
