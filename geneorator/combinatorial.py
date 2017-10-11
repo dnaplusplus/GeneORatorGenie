a31b2f5ca69d08dc2c97eda8aed5518996f056fd
Neil Swainston <neil.swainston@manchester.ac.uk>
2017-10-11 14:24:16 +0100

    Ensured even number of oligos in design and in blocks.

--- geneorator/combinatorial.py
+++ geneorator/combinatorial.py
@@ -9,11 +9,14 @@ import itertools
 import sys
 
 
-def combine(oligos, n_mutated, n_blocks):
+def combine(oligos, n_mutated=0, n_blocks=1):
     '''Design combinatorial assembly.'''
-    designs = []
 
-    len_block = len(oligos) / n_blocks
+    # Assertion sanity checks:
+    assert(len(oligos) % 2 == 0)
+    assert(len(oligos) / n_blocks >= 2)
+
+    designs = []
 
     # Get combinations:
     combis = itertools.combinations(oligos, n_mutated)
@@ -24,15 +27,36 @@ def combine(oligos, n_mutated, n_blocks):
         for var in combi:
             design[design.index(var)] = var + 'm'
 
-        designs.append([design[i:i + len_block]
-                        for i in xrange(0, len(design), len_block)])
+        block_lengths = [0] * n_blocks
+
+        for idx in itertools.cycle(range(0, n_blocks)):
+            block_lengths[idx] = block_lengths[idx] + 2
+
+            if sum(block_lengths) == len(design):
+                break
+
+        idx = 0
+        blocks = []
+
+        for val in block_lengths:
+            blocks.append(design[idx: idx + val])
+            idx = idx + val
+
+        designs.append(blocks)
 
     return designs
 
 
+def _pairwise(iterable):
+    '''s -> (s0,s1), (s1,s2), (s2, s3), ...'''
+    a, b = itertools.tee(iterable)
+    next(b, None)
+    return zip(a, b)
+
+
 def main(args):
     '''main method.'''
-    oligos = [str(val) for val in range(1, 28)]
+    oligos = [str(val) for val in range(1, 7)]
 
     for design in combine(oligos, int(args[0]), int(args[1])):
         print design
